d275106b75bf1f37bc5093939c3833ac7e2ee3ca
Fang Chen <fanche@microsoft.com>
2020-03-09 15:39:02 -0700

    first

---
+++ jwt_demo2/Controllers/UserModel.cs
@@ -0,0 +1,9 @@
+﻿namespace jwt_demo2.Controllers
+{
+    public class UserModel
+    {
+        public string Username { get; set; }
+        public string EmailAddress { get; set; }
+        public System.DateTime DateOfJoing { get; set; }
+    }
+}
